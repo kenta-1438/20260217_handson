601b9d53ee1c70bf97b60b9b8a83355b5d39dba2
kenta-1438 <isshy.zawar@gmail.com>
2026-01-27 14:54:50 +0900

    Update sample10.py

--- sample10.py
+++ sample10.py
@@ -1,3 +1,6 @@
+# PCの表示画面の中から,あらかじめ登録しておいた画像（ここでは, Excel.png とする）が
+# 表示されている部分を探して,ダブルクリックする。
+
 import pyautogui as pag
 import time
 
@@ -5,3 +8,4 @@ p = pag.locateOnScreen("Excel.png",confidence=0.35)
 x,y=pag.center(p)
 time.sleep(1)
 pag.doubleClick(x,y,button="left")
+
