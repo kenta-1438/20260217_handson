74efab117f3c05e9f983744b5c9985f244fb919e
kenta-1438 <isshy.zawar@gmail.com>
2026-01-27 09:20:59 +0900

    Update sample4.py

--- sample4.py
+++ sample4.py
@@ -1,5 +1,9 @@
+# プログラム開始時のマウスカーソルの場所を取得する。
+# 続いて1秒かけて,(300,300)にマウスカーソルを移動する。
+
 import pyautogui as pag
 
 print(f"移動前:{pag.position()}")
 pag.moveTo(300,300,duration=1)
+
 print(f"移動後:{pag.position()}")
